2ee6f16af65d4b6e7ee2815b3e67ab840e32815a
Dominic Järmann <dominic.jaermann@icloud.com>
2019-10-11 10:56:33 +0200

    User can now add a Friend only once

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,14 @@
 class FriendshipsController < ApplicationController
     def create
-    @friendship = Pilot.find(current_user.id).friendships.build(:friend_id => params[:friend_id])
+    pilot = Pilot.find(current_user.id)
+    @friendship = pilot.friendships.build(:friend_id => params[:friend_id])
+ 
+    if pilot.friendships.where(:pilot_id => pilot.id, :friend_id  => params[:friend_id]).exists?
+      flash[:alert] = "You have already added " + Pilot.find(params[:friend_id]).name + " " + Pilot.find(params[:friend_id]).lastname +  " as a friend."
+      redirect_to root_url
+      return
+    end
+
     if @friendship.save
       flash[:notice] = "Added friend."
       redirect_to root_url
